6eaaabf04484f12e72fe568b9206c3ff02a72898
ivamtra <ivanmar009@gmail.com>
2022-06-08 15:48:11 +0000

    comment komin inn en alls stadar

--- web/src/components/Article/Article.js
+++ web/src/components/Article/Article.js
@@ -1,5 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import CommentsCell from 'src/components/CommentsCell'
+import CommentForm from 'src/components/CommentForm'
 
 const Article = ({ article }) => {
   return (
@@ -10,7 +11,12 @@ const Article = ({ article }) => {
         </h2>
       </header>
       <div className="mt-2 text-gray-900 font-light">{article.body}</div>
-      {<CommentsCell />}
+      <div className="mt-12">
+        <CommentForm postId={article.id} />
+        <div className="mt-12">
+          <CommentsCell postId={article.id} />
+        </div>
+      </div>
     </article>
   )
 }
